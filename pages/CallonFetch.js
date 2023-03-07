3b060b61eeebe736bc2b59e22673b56b63f7cfd7
Rudra Narayan Boitei <rudra2021@gift.edu.in>
2023-03-07 12:06:42 +0530

    commit

--- pages/CallonFetch.js
+++ pages/CallonFetch.js
@@ -15,8 +15,8 @@ export const CallonFetch = () => {
     }
   return (
     <div>
-        <h1 className='text-6xl font-bold '>{data?.setup}</h1>
-        <h1 className='text-6xl font-bold text-blue-600 mt-8'>{data?.delivery}</h1>
+        <h1 className='text-3xl md:text-6xl font-bold '>{data?.setup}</h1>
+        <h1 className='text-3xl md:text-6xl font-bold text-blue-600 mt-8'>{data?.delivery}</h1>
         <button className='text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-blue-300 font-medium rounded-full tex-sm px-5 py-2.5 text-center mt-9 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800' onClick={refatch}>Click Me</button>
     </div>
   )
